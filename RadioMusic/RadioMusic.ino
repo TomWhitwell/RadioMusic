900193929571fb991521f687b922ab9123ec98a8
TomWhitwell <tom.whitwell@gmail.com>
2014-10-13 22:02:17 +0100

    Read SD File Directories into array

--- RadioMusic/RadioMusic.ino
+++ RadioMusic/RadioMusic.ino
@@ -25,7 +25,16 @@ AudioOutputAnalog        dac1;           //xy=334,98
 AudioConnection          patchCord1(playRaw1, dac1);
 // GUItool: end automatically generated code
 
-
+// SETUP FILES TO STORE DETAILS OF FILES ON THE SD CARD 
+#define MAX_FILES 50
+#define BANKS 8
+String FILE_TYPE = "WAV";
+String FILE_NAMES [BANKS][MAX_FILES];
+String FILE_DIRECTORIES[BANKS][MAX_FILES];
+unsigned long FILE_SIZES[BANKS][MAX_FILES];
+int FILE_COUNT[BANKS];
+String CURRENT_DIRECTORY = "0";
+File root;
 
 
 void setup() {
@@ -47,6 +56,11 @@ void setup() {
       delay(500);
     }
   }
+
+// OPEN SD CARD AND SCAN FILES INTO DIRECTORY ARRAYS 
+root = SD.open("/");  
+scanDirectory(root, 0);
+  
 }
 
 
@@ -55,8 +69,11 @@ void setup() {
 
 void loop() {
 
-  int pot = ReadAndAverage(2,64);
+int pot = ReadAndAverage(2,64);
 
+
+
+ 
 
 
 
@@ -66,6 +83,10 @@ void loop() {
 
 
 
+
+
+
+// READ AVERAGE VALUES FROM NAMED ANALOG INPUT 
 int ReadAndAverage (int input_number, int average_count){
   int values;
   for(int i = 0; i < average_count; i++){
@@ -73,5 +94,34 @@ int ReadAndAverage (int input_number, int average_count){
   }
   values = values / average_count;
   return values;
+}
+
+
+// SCAN SD DIRECTORIES INTO ARRAYS 
+void scanDirectory(File dir, int numTabs) {
+  while(true) {  
+    File entry =  dir.openNextFile();
+    if (! entry) {
+      // no more files
+      break;
+    }
+    String fileName = entry.name();
+
+    if (fileName.endsWith(FILE_TYPE) && fileName.startsWith("_") == 0){
+      int intCurrentDirectory = CURRENT_DIRECTORY.toInt();
+      FILE_NAMES[intCurrentDirectory][FILE_COUNT[intCurrentDirectory]] = entry.name();
+      FILE_SIZES[intCurrentDirectory][FILE_COUNT[intCurrentDirectory]] = entry.size();
+      FILE_DIRECTORIES[intCurrentDirectory][FILE_COUNT[intCurrentDirectory]] = CURRENT_DIRECTORY;
+      FILE_COUNT[intCurrentDirectory]++;
+
+    };
+
+    // Ignore OSX Spotlight and Trash Directories 
+    if (entry.isDirectory() && fileName.startsWith("SPOTL") == 0 && fileName.startsWith("TRASH") == 0) {
+      CURRENT_DIRECTORY = entry.name();
+      scanDirectory(entry, numTabs+1);
+    } 
+    entry.close();
+  }
 }
 
