89b39ce2c9d8dba43c8c484bf83ca371a7e087f1
TomWhitwell <tom.whitwell@gmail.com>
2015-06-25 17:39:55 +0100

    comments on RM_blink

--- Collateral/alt.firmware/RM_Blink/RM_Blink.ino
+++ Collateral/alt.firmware/RM_Blink/RM_Blink.ino
@@ -5,11 +5,8 @@
   This example code is in the public domain.
  */
  
-// Pin 13 has an LED connected on most Arduino boards.
-// Pin 11 has the LED on Teensy 2.0
-// Pin 6  has the LED on Teensy++ 2.0
-// Pin 13 has the LED on Teensy 3.0
-// give it a name:
+// In the Radio Music Hardware,  Bank LEDs are 3,4,5,6, Reset LED is 11. 
+// Choose an LED and give it a name:
 int led = 3;
 
 // the setup routine runs once when you press reset:
